d845f32eab68145f4ed22947c95c0f76463a1a85
LYJ09X <58910154+LYJ09X@users.noreply.github.com>
2022-03-30 19:48:51 +0800

    Update and rename DD.lua to Ding.lua

--- Ding.lua
+++ Ding.lua
@@ -44,10 +44,10 @@ function wa_lua_on_handshake_cb(ctx)
     if flags[uuid] ~= kHttpHeaderSent then
         local host = ctx_address_host(ctx)
         local port = ctx_address_port(ctx)
-        local res = 'CONNECT ' .. host .. ':' .. port .. '@tms.dingtalk.com:80 HTTP/1.1\r\n' ..
-                    'Host: tms.dingtalk.com:80\r\n' ..
+        local res = 'CONNECT ' .. host .. ':' .. port .. '@tls-long.dingtalk.com:80 HTTP/1.1\r\n' ..
+                    'Host:down.dingtalk.com:80\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
-                    'User-Agent: okhttp/4.9.0 Dalvik/2.1.0 baiduboxapp/11.0.5.12 (Baidu; P1 11)\r\n'..
+                    'User-Agent:baiduboxapp\r\n'..
                     'Proxy-Connection: Keep-Alive\r\n'..
                     'X-T5-Auth: YTY0Nzlk\r\n\r\n'
         ctx_write(ctx, res)
